b2a1f51a161bc90cd62822fe113e56feac9324e8
Jesse Lee <jqlee85@gmail.com>
2023-06-29 13:37:59 -0700

    Add any type.

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -6,7 +6,7 @@ const Sidebar: React.FC<{quotes:any}> = ( { quotes }) => {
   return (
     <aside className="md:max-w-xs lg:max-w-1/3 p-4 overflow-y-auto">
       <h2>Misc Quotes</h2>
-      {quotes.map(quote => (
+      {quotes.map((quote:any) => (
         <Quote  
           quoteObj={quote}
           key={`quote_${quote.id}`}
